557e0e6652d955599537770b5f2b37d350733dbf
Artem Bubnov <artem.bubnov@vrpconsulting.com>
2022-12-28 16:27:46 +0300

    diff commit 8

--- build/deploy_to_developer_sf_org.sh
+++ build/deploy_to_developer_sf_org.sh
@@ -7,7 +7,7 @@ echo $BRANCH_NAME
 echo $SOURCE_BRANCH_NAME
 
 echo "Find the difference between organizations"
-#PATH_FILTER="./"
-#UPDATED_FILES=$(git diff --name-only origin/dev force-app)
-#echo "What is the diff?"
-#echo $UPDATED_FILES
+PATH_FILTER="./"
+UPDATED_FILES=$(git diff --name-only origin/qa force-app)
+echo "What is the diff?"
+echo $UPDATED_FILES
