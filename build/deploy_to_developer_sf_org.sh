f13fd86e66ebfc003e98ed63a3e5aef3da35159b
Artem Bubnov <artem.bubnov@vrpconsulting.com>
2022-12-30 11:46:22 +0300

    bash commit 4

--- build/deploy_to_developer_sf_org.sh
+++ build/deploy_to_developer_sf_org.sh
@@ -13,14 +13,10 @@ echo $SOURCE_BRANCH_NAME
 
 echo "Find the difference between organizations"
 PATH_FILTER="./"
-UPDATED_FILES=$(git diff --name-only origin/qa force-app)
+UPDATED_FILES=$(git diff --name-only origin/dev force-app)
 echo "What is the diff?"
 echo $UPDATED_FILES
 
 
 echo "Test deploy to Salesforce env without saving"
-sfdx force:source:deploy -p "$UPDATED_FILES" -c -l RunLocalTests -u salesforce_build.org --loglevel WARN
-
-
-
-#sfdx force:source:deploy -p force-app -c -l RunLocalTests -u salesforce_test.org --loglevel WARN
+#sfdx force:source:deploy -p "$UPDATED_FILES" -c -l RunLocalTests -u salesforce_build.org --loglevel WARN
