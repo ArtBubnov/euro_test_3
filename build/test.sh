b36aaf98b99ab4344f729f8afdc1889508c9498b
Artem Bubnov <artem.bubnov@vrpconsulting.com>
2023-01-03 17:29:47 +0300

    bash commit 15

--- build/test.sh
+++ build/test.sh
@@ -1,9 +1,16 @@
 echo -e "test\n\n\n"
 CIRCLE_TOKEN=7a87730dbe7ce5fdc8b4c5129bf095ad2d6d954a
-TEST=curl -H "Circle-Token: $CIRCLE_TOKEN" https://circleci.com/api/v2/project/gh/ArtBubnov/euro_test_3/$CIRCLE_BUILD_NUM/artifacts \
-        | grep -o "https://[^"]*" \
-        | wget --verbose --header "Circle-Token: $CIRCLE_TOKEN" --input-file -
 
-cat $TEST
+echo "*******************"
+echo $CIRCLE_BUILD_NUM
+echo "*******************"
 
-echo "------------"
+
+#TEST=
+#curl -H "Circle-Token: 7a87730dbe7ce5fdc8b4c5129bf095ad2d6d954a" https://circleci.com/api/v2/project/gh/ArtBubnov/euro_test_3/$CIRCLE_BUILD_NUM/artifacts \
+#        | grep -o "https://[^"]*" \
+#        | wget --verbose --header "Circle-Token: $CIRCLE_TOKEN" --input-file -
+
+#cat $TEST
+
+#echo "------------"
