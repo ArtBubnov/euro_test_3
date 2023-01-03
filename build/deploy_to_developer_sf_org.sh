f5ebfb833a5981eba31a9f68bd30b71006b165eb
Artem Bubnov <artem.bubnov@vrpconsulting.com>
2023-01-03 15:06:36 +0300

    bash commit 8

--- build/deploy_to_developer_sf_org.sh
+++ build/deploy_to_developer_sf_org.sh
@@ -8,7 +8,35 @@ echo $SOURCE_BRANCH_NAME
 
 UPDATED_FILES=""
 
-
+echo "Select diff_file and Salesforce org alias"
+case $TARGET_BRANCH_NAME in
+    "dev")
+        CASE_LOG="dev"
+        DIFF_FILE_NAME="devdiff"
+        SALESFORCE_ORG_ALIAS="salesforce_dev.org"
+        ;;
+    "qa")
+        CASE_LOG="qa"
+        DIFF_FILE_NAME="qadiff"
+        SALESFORCE_ORG_ALIAS="salesforce_qa.org"
+        ;;
+    "uat")
+        CASE_LOG="uat"
+        DIFF_FILE_NAME="uatdiff"
+        SALESFORCE_ORG_ALIAS="salesforce_uat.org"
+        ;;
+    "main")
+        CASE_LOG="prod"
+        DIFF_FILE_NAME="maindiff"
+        SALESFORCE_ORG_ALIAS="salesforce_prod.org"
+        ;;
+    *)
+        echo "Not valid"
+        ;;
+esac
+echo CASE_LOG
+echo DIFF_FILE_NAME
+echo SALESFORCE_ORG_ALIAS
 
 
 
@@ -18,6 +46,12 @@ PATH_FILTER="./"
 UPDATED_FILES=$(git diff --name-only origin/dev force-app)
 echo "What is the diff?"
 echo $UPDATED_FILES
+
+echo "Place diff data to a file"
+echo $UPDATED_FILES | tr '\n' ',' | sed 's/\(.*\),/\1 /'>>$DIFF_FILE_NAME.txt
+
+echo "RESUL TEST"
+cat $DIFF_FILE_NAME.txt
 
 
 echo "Test deploy to Salesforce env without saving"
