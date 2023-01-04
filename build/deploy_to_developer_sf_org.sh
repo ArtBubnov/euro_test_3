11344151ae444e4a29f5600b44919bb1b763f7b8
Artem Bubnov <artem.bubnov@vrpconsulting.com>
2023-01-04 15:03:17 +0300

    commit

--- build/deploy_to_developer_sf_org.sh
+++ build/deploy_to_developer_sf_org.sh
@@ -41,20 +41,20 @@ echo $SALESFORCE_ORG_ALIAS
 
 
 
-echo -e "\n\n\nFind the difference between organizations"
-PATH_FILTER="./"
-UPDATED_FILES=$(git diff --name-only origin/dev force-app)
-echo "What is the diff?"
-echo $UPDATED_FILES
+#echo -e "\n\n\nFind the difference between organizations"
+#PATH_FILTER="./"
+#UPDATED_FILES=$(git diff --name-only origin/dev force-app)
+#echo "What is the diff?"
+#echo $UPDATED_FILES
 
-echo $UPDATED_FILES>>$DIFF_FILE_NAME.txt
-echo -e "\n\n\nPlace diff data to a file"
-mkdir /tmp/artifacts
-echo -e $UPDATED_FILES | tr '\n' ',' | sed 's/\(.*\),/\1 /'>/tmp/artifacts/artifact-2
+#echo $UPDATED_FILES>>$DIFF_FILE_NAME.txt
+#echo -e "\n\n\nPlace diff data to a file"
+#mkdir /tmp/artifacts
+#echo -e $UPDATED_FILES | tr '\n' ',' | sed 's/\(.*\),/\1 /'>/tmp/artifacts/artifact-2
 
-echo -e "\n\n\nRESUL TEST"
-cat /tmp/artifacts/artifact-2
+#echo -e "\n\n\nRESUL TEST"
+#cat /tmp/artifacts/artifact-2
 
 
-echo "Test deploy to Salesforce env without saving"
-#sfdx force:source:deploy -p "$UPDATED_FILES" -c -l RunLocalTests -u salesforce_build.org --loglevel WARN
+echo "Test deploy to Salesforce env \n\n\n"
+#sfdx force:source:deploy -p "$UPDATED_FILES" -l RunLocalTests -u salesforce_build.org --loglevel WARN
--- build/install_dependencies.sh
+++ build/install_dependencies.sh
@@ -1,7 +1,7 @@
 #This installs the sfdx cli
 
-echo "****   Installing Salesforce CLI   ****"
+echo "Installing Salesforce CLI"
 sudo npm install -global sfdx-cli
 
-echo "****   Salesforce CLI version check   ****"
+echo "Salesforce CLI version check"
 sudo npm install sfdx --version
