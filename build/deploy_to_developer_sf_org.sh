539fe0a60436f8a05f557f7ad29ccdd812ccfcf1
Artem Bubnov <artem.bubnov@vrpconsulting.com>
2023-01-18 16:34:06 +0300

    LOG and diff test 10

--- build/deploy_to_developer_sf_org.sh
+++ build/deploy_to_developer_sf_org.sh
@@ -3,13 +3,15 @@ echo -e "Deploy data to Dev Env\n\n\n"
 echo -e "New tests\n\n\n"
 echo "git log"
 echo $(git log --oneline --no-decorate --merges qa)
+ehco -e "\n\n\n"
 
 getBranchPRInfo=$((git log --oneline --no-decorate --merges qa)| cut -d\  -f1)
+test=$($getBranchPRInfo| cut -d\  -f1)
 
 #echo $(git log --oneline --no-decorate --merges qa)| cut -d\  -f1 > getBranchPRInfo
 
 echo -e "ID IS"
-echo $getBranchPRInfo
+echo $test
 
 
 #echo $(git log --oneline qa) > gitLog.txt
