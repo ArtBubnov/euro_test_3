33bd1fa7b11c108b0fa59e6c3bae98a5e0de6500
Artem Bubnov <artem.bubnov@vrpconsulting.com>
2023-01-23 15:18:27 +0300

    diff logic 37

--- build/pre_deploy_actions.sh
+++ build/pre_deploy_actions.sh
@@ -10,22 +10,22 @@ case $TARGET_BRANCH_NAME in
     "dev")
         CASE_LOG="dev"
         ACCESS_KEY_SF=$ACCESS_KEY_SF_DEV
-        SALESFORCE_ORG_ALIAS="salesforce_dev.org"
+        SALESFORCE_TARGET_ORG_ALIAS="salesforce_dev.org"
         ;;
     "qa")
         CASE_LOG="qa"
         ACCESS_KEY_SF=$ACCESS_KEY_SF_QA
-        SALESFORCE_ORG_ALIAS="salesforce_qa.org"
+        SALESFORCE_TARGET_ORG_ALIAS="salesforce_qa.org"
         ;;
     "uat")
         CASE_LOG="uat"
         ACCESS_KEY_SF=$ACCESS_KEY_SF_UAT
-        SALESFORCE_ORG_ALIAS="salesforce_uat.org"
+        SALESFORCE_TARGET_ORG_ALIAS="salesforce_uat.org"
         ;;
     "main")
         CASE_LOG="prod"
         ACCESS_KEY_SF=$ACCESS_KEY_SF_PROD
-        SALESFORCE_ORG_ALIAS="salesforce_prod.org"
+        SALESFORCE_TARGET_ORG_ALIAS="salesforce_prod.org"
         ;;
     *)
         echo "Not valid"
@@ -37,8 +37,14 @@ echo $CASE_LOG
 echo "Salesforce alias to be used:"
 echo $SALESFORCE_ORG_ALIAS
 
-echo "Rin apex tests \n\n\n"
-sfdx force:apex:test:run -l RunLocalTests -d $SOURCE_BRANCH_NAME/force-app -u $SALESFORCE_ORG_ALIAS
+
+
+
+
+
+
+#echo "Rin apex tests \n\n\n"
+#sfdx force:apex:test:run -l RunLocalTests -d ${SOURCE_BRANCH_NAME}/force-app -u $SALESFORCE_ORG_ALIAS
 
 echo "Test deploy to Salesforce env without saving \n\n\n"
 sfdx force:source:deploy -p $TARGET_BRANCH_NAME/force-app -c -l RunLocalTests -u $SALESFORCE_ORG_ALIAS --loglevel WARN
