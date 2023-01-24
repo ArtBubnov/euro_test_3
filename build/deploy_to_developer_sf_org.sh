7fd7d0cde751b0772539731ab5afb38b3db1c596
Artem Bubnov <artem.bubnov@vrpconsulting.com>
2023-01-24 14:38:42 +0300

    diff logic 81

--- build/deploy_to_developer_sf_org.sh
+++ build/deploy_to_developer_sf_org.sh
@@ -71,7 +71,7 @@ git branch
 
 
 #DIFF_BRANCH=$(git branch --contains ${GET_PREVIOUS_MERGE_ID})
-DIFF_BRANCH=$((git branch -r --contains 7cf1acc)| tr ' ' '' | cut -d\  -f1)
+DIFF_BRANCH=$((git branch -r --contains 7cf1acc)| cut -d\  -f1)
 echo -e "\n the RESULT:"
 git branch -r --contains 7cf1acc
 echo $DIFF_BRANCH
