66f3ef467cd4fadb5e8d173c6a852e5ed751aca7
Artem Bubnov <artem.bubnov@vrpconsulting.com>
2023-02-13 17:18:01 +0300

    test_09

--- build/pre_deploy_actions.sh
+++ build/pre_deploy_actions.sh
@@ -81,48 +81,49 @@ echo -e "\n\n\nFiles to deploy:"
 echo $FILES_TO_DEPLOY
 
 
-
-#get to classes directory to define the list of tests to be executed
-cd force-app/main/default/classes/tests
+#---------------------------temp comment--------------------------------------
+##get to classes directory to define the list of tests to be executed
+#cd force-app/main/default/classes/tests
 
 #add all the files in the folder into array
-mapfile -t classes_files_array < <( ls )
+#mapfile -t classes_files_array < <( ls )
 
 #define which of the files are tests
-COUNT=0
-ARRAY_LEN=${#classes_files_array[@]}
-LIST_OF_FILES_TO_TEST=""
-LOOP_LEN=$( expr $ARRAY_LEN - 1)
+#COUNT=0
+#ARRAY_LEN=${#classes_files_array[@]}
+#LIST_OF_FILES_TO_TEST=""
+#LOOP_LEN=$( expr $ARRAY_LEN - 1)
 
 
-while [ $COUNT -le $LOOP_LEN ]
-do
-    if [[ ${classes_files_array[$COUNT]} == *"Test.cls"* ]];
-    then
+#while [ $COUNT -le $LOOP_LEN ]
+#do
+#    if [[ ${classes_files_array[$COUNT]} == *"Test.cls"* ]];
+#    then
 
-        if [[ ${classes_files_array[$COUNT]} == *"cls-meta.xml"* ]];
-        then
-            LIST_OF_XML_FILES=$LIST_OF_XML_FILES{classes_files_array[$COUNT]}","
-        else
-            LEN_OF_FILE_NAME=${#classes_files_array[$COUNT]}
-            NUMBER_OF_SYMBOLS_TO_TRUNCATE=$( expr $LEN_OF_FILE_NAME - 4 )
-            FILE_NAME_TRUNC=$((echo ${classes_files_array[$COUNT]}) | cut -c 1-$NUMBER_OF_SYMBOLS_TO_TRUNCATE )
-            LIST_OF_FILES_TO_TEST=$LIST_OF_FILES_TO_TEST$FILE_NAME_TRUNC","
-        fi
+#        if [[ ${classes_files_array[$COUNT]} == *"cls-meta.xml"* ]];
+#        then
+#            LIST_OF_XML_FILES=$LIST_OF_XML_FILES{classes_files_array[$COUNT]}","
+#        else
+#            LEN_OF_FILE_NAME=${#classes_files_array[$COUNT]}
+#            NUMBER_OF_SYMBOLS_TO_TRUNCATE=$( expr $LEN_OF_FILE_NAME - 4 )
+#            FILE_NAME_TRUNC=$((echo ${classes_files_array[$COUNT]}) | cut -c 1-$NUMBER_OF_SYMBOLS_TO_TRUNCATE )
+#            LIST_OF_FILES_TO_TEST=$LIST_OF_FILES_TO_TEST$FILE_NAME_TRUNC","
+#        fi
 
-    fi 
-    COUNT=$(( $COUNT +1))
-done
+#    fi 
+#    COUNT=$(( $COUNT +1))
+#done
 
 
-LEN_OF_LIST_OF_FILES_TO_TEST=${#LIST_OF_FILES_TO_TEST}
+#LEN_OF_LIST_OF_FILES_TO_TEST=${#LIST_OF_FILES_TO_TEST}
 
-NUMBER_OF_SYMBOLS_TO_TRUNCATE=$( expr $LEN_OF_LIST_OF_FILES_TO_TEST - 1 )
+#NUMBER_OF_SYMBOLS_TO_TRUNCATE=$( expr $LEN_OF_LIST_OF_FILES_TO_TEST - 1 )
 
-LIST_OF_FILES_TO_TEST_TRUNC=$((echo ${LIST_OF_FILES_TO_TEST}) | cut -c 1-$NUMBER_OF_SYMBOLS_TO_TRUNCATE )
+#LIST_OF_FILES_TO_TEST_TRUNC=$((echo ${LIST_OF_FILES_TO_TEST}) | cut -c 1-$NUMBER_OF_SYMBOLS_TO_TRUNCATE )
 
-echo -e "\n\n\nList of files to test\n\n\n"
-echo $LIST_OF_FILES_TO_TEST_TRUNC
+#echo -e "\n\n\nList of files to test\n\n\n"
+#echo $LIST_OF_FILES_TO_TEST_TRUNC
+#---------------------------temp comment--------------------------------------
 
 #get back to the root directory
 cd /home/circleci/project
