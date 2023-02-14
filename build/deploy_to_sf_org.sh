49186e32c444a11bc92b92de8e291d1170393298
Artem Bubnov <artem.bubnov@vrpconsulting.com>
2023-02-14 15:33:17 +0300

    test_37

--- build/deploy_to_sf_org.sh
+++ build/deploy_to_sf_org.sh
@@ -125,3 +125,168 @@ git checkout $ORIGIN_BRANCH
 
 echo "Deploy data to Dev Env"
 #sfdx force:source:deploy -p ${FILES_TO_DEPLOY} -u ${SALESFORCE_ORG_ALIAS} --loglevel WARN
+
+
+
+
+
+
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@"
+#test. delete
+FILES_TO_DEPLOY="force-app/main/default"
+#test. delete
+echo "loop start"
+if [[ $FILES_TO_DEPLOY == *"force-app/main/default"* ]];
+then
+#-------------------TRUE logic start------------------------------------
+    echo "TRUE"
+    STRING_NESTED_FILES_1=""
+
+
+    #get into force-app/main/default
+    cd force-app/main/default
+    #get list of folders nested in force-app/main/default
+    mapfile -t forceapp_files_array < <( ls )
+    LOOP_ROOT_DIR="/home/circleci/project/force-app/main/default"
+
+    COUNT=0
+    ARRAY_LEN=${#forceapp_files_array[@]}
+    LIST_OF_FILES_TO_TEST=""
+    LOOP_LEN=$( expr $ARRAY_LEN - 1)
+
+    # start of the loop to check each folder in force-app/main/default
+    while [ $COUNT -le $LOOP_LEN ]
+    do
+        cd ${forceapp_files_array[$COUNT]}
+        echo -e "\n\n\nLOOP STEP START"
+        echo -e "\nName of the folder"
+        echo ${forceapp_files_array[$COUNT]}
+        echo -e "\nWhat is in the foleder\n"
+        ls -a
+        mapfile -t current_folder_files_array < <( ls -a )
+
+
+#nesting level 01----------------------------------------------------------------
+        COUNT_02=2
+        ARRAY_LEN_02=${#current_folder_files_array[@]}
+        LIST_OF_FILES_TO_TEST_02=""
+        LOOP_LEN_02=$( expr $ARRAY_LEN_02 - 1)
+
+        while [ $COUNT_02 -le $LOOP_LEN_02 ]
+        do
+            if [[ -f ${current_folder_files_array[$COUNT_02]} ]];
+            then
+                echo -e "\n***"
+                echo "FILE IS"
+                echo ${current_folder_files_array[$COUNT_02]}
+                echo "THIS IS FILE"
+                echo -e "***\n"
+                STRING_NESTED_FILES_1=$STRING_NESTED_FILES_1${current_folder_files_array[$COUNT_02]}","
+            else
+                echo -e "\n***"
+                echo "FILE IS"
+                echo ${current_folder_files_array[$COUNT_02]}
+                echo "THIS IS DIRECTORY"
+                echo -e "***\n"
+
+#nesting level 02--------------------------------------------------------------------
+                cd ${current_folder_files_array[$COUNT_02]}
+                mapfile -t current_folder_files_array_02 < <( ls -a )
+
+                COUNT_03=2
+                ARRAY_LEN_03=${#current_folder_files_array_02[@]}
+                LIST_OF_FILES_TO_TEST_03=""
+                LOOP_LEN_03=$( expr $ARRAY_LEN_03 - 1)
+
+                while [ $COUNT_03 -le $LOOP_LEN_03 ]
+                do
+                    if [[ -f ${current_folder_files_array_02[$COUNT_02]} ]];
+                    then
+                        echo -e "\n***"
+                        echo "NEST 2"
+                        echo "FILE IS"
+                        echo ${current_folder_files_array_02[$COUNT_02]}
+                        echo "THIS IS FILE"
+                        echo -e "***\n"
+                    else
+                        echo -e "\n***"
+                        echo "NEST 2"
+                        echo "FILE IS"
+                        echo ${current_folder_files_array_02[$COUNT_02]}
+                        echo "THIS IS DIRECTORY"
+                        echo -e "***\n"
+                    fi
+                    COUNT_03=$(( $COUNT_03 +1))
+                done
+
+
+
+
+
+
+
+            fi 
+
+            COUNT_02=$(( $COUNT_02 +1))
+        done
+
+
+
+        cd $LOOP_ROOT_DIR
+        echo -e "\nLOOP STEP END\n\n\n"
+        COUNT=$(( $COUNT +1))
+    done
+#-------------------TRUE logic start------------------------------------
+else
+    echo "FALSE"
+fi
+
+
+echo "STRING_NESTED_FILES_1"
+echo $STRING_NESTED_FILES_1
+LEN_OF_STRING_NESTED_FILES_1=${#STRING_NESTED_FILES_1}
+NUMBER_OF_SYMBOLS_TO_TRUNCATE=$( expr $LEN_OF_STRING_NESTED_FILES_1 - 1 )
+STRING_NESTED_FILES_1_TRUNC=$((echo ${STRING_NESTED_FILES_1}) | cut -c 1-$NUMBER_OF_SYMBOLS_TO_TRUNCATE )
+
+echo -e "\n\n\nSTRING_NESTED_FILES_1_TRUNC\n\n\n"
+echo $STRING_NESTED_FILES_1_TRUNC
+
+
+IFS=',' read -r -a string_nested_files_1_trunc_array <<< "$STRING_NESTED_FILES_1_TRUNC"
+
+
+COUNT_NEST_1=0
+ARRAY_LEN_NEST_1=${#string_nested_files_1_trunc_array[@]}
+echo $ARRAY_LEN_NEST_1
+LIST_OF_FILES_TO_TEST_NEST_1=""
+LOOP_LEN_NEST_1=$( expr $ARRAY_LEN_NEST_1 - 1)
+COLLECT_XML_FILES=""
+
+
+while [ $COUNT_NEST_1 -le $LOOP_LEN_NEST_1 ]
+do
+    if [[ ${string_nested_files_1_trunc_array[$COUNT_NEST_1]} == *".xml"* ]];
+    then
+        echo "TRUE"
+        echo ${string_nested_files_1_trunc_array[$COUNT_NEST_1]}
+        COLLECT_XML_FILES=$COLLECT_XML_FILES${string_nested_files_1_trunc_array[$COUNT_NEST_1]}","
+    else
+        echo "FALSE"
+        echo ${string_nested_files_1_trunc_array[$COUNT_NEST_1]}
+    fi
+    COUNT_NEST_1=$(( $COUNT_NEST_1 +1))
+done
+
+LEN_OF_COLLECT_XML_FILES=${#COLLECT_XML_FILES}
+NUMBER_OF_SYMBOLS_TO_TRUNCATE=$( expr $LEN_OF_COLLECT_XML_FILES - 1 )
+STRING_NESTED_FILES_1_TRUNC=$((echo ${COLLECT_XML_FILES}) | cut -c 1-$NUMBER_OF_SYMBOLS_TO_TRUNCATE )
+
+echo -e "\n\n\nFINAL ___ STRING_NESTED_FILES_1_TRUNC-------\n\n\n"
+echo $STRING_NESTED_FILES_1_TRUNC
+
+
+
+
+
+
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@"
