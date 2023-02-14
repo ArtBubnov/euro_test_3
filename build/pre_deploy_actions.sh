925b99b9a710633038e3752eb179a1c5074509cf
Artem Bubnov <artem.bubnov@vrpconsulting.com>
2023-02-14 12:20:16 +0300

    test_28

--- build/pre_deploy_actions.sh
+++ build/pre_deploy_actions.sh
@@ -251,7 +251,7 @@ echo -e "\n\n\nSTRING_NESTED_FILES_1_TRUNC\n\n\n"
 echo $STRING_NESTED_FILES_1_TRUNC
 
 
-IFS=', ' read -r -a string_nested_files_1_trunc_array <<< "$STRING_NESTED_FILES_1_TRUNC"
+IFS=',' read -r -a string_nested_files_1_trunc_array <<< "$STRING_NESTED_FILES_1_TRUNC"
 
 COUNT_NEST_1=0
 ARRAY_LEN_NEST_1=${#string_nested_files_1_trunc_array[@]}
