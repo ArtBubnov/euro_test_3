c7c13f275a1a6e99821932c6e0cccf2e4c2702e8
Artem Bubnov <artem.bubnov@vrpconsulting.com>
2023-02-14 17:48:56 +0300

    test_40

--- build/test.sh
+++ build/test.sh
@@ -108,7 +108,7 @@ then
                                 echo ${current_folder_files_array_03[$COUNT_04]}
                                 echo "THIS IS FILE"
                                 echo -e "!!!\n"
-                                STRING_NESTED_FILES_3=$STRING_NESTED_FILES_2${forceapp_files_array[$COUNT]}"-->"${current_folder_files_array[$COUNT_02]}"-->"${current_folder_files_array_02[$COUNT_03]}"/"${current_folder_files_array_03[$COUNT_04]}","
+                                STRING_NESTED_FILES_3=$STRING_NESTED_FILES_2${forceapp_files_array[$COUNT]}" -->"${current_folder_files_array[$COUNT_02]}"-->"${current_folder_files_array_02[$COUNT_03]}"/"${current_folder_files_array_03[$COUNT_04]}","
                             else
                                 echo -e "\n!!!"
                                 echo "NEST 2"
